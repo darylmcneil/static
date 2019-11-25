4ee0d91befe34d77f0026882bdbd40d49031e011
darylmcneil <50683412+darylmcneil@users.noreply.github.com>
2019-11-25 12:41:03 -0500

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,14 +1,20 @@
-pipeline {
-  agent any 
-  stages {
-    stage(‘Build’) {
-      steps {
-        sh ‘echo “Hello World”’
-        sh ‘“
-                  echo “Multiline shell steps works too”
-                  ls -lah
-               “‘
-      }
+pipeline{
+    agent any
+    stages {
+        stage('Lint HTML') {
+            steps {
+                sh 'tidy -q -e *.html'
+            }
+        stage('Upload to AWS') {
+            steps {
+                withAWS(region:
+                'us-east-1',credentials:'blueocean') {
+                    s3Upload(pathStyleAccessEnabled:true, payloadSigningEnabled: true, file:'index.html', bucket:'c3pipelines')
+                }
+            }
+        }
+            }
+        }
     }
-  }
+}
 }
